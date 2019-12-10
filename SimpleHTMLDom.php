e7ed81dc4e300d32e7e492a9825d99040ed6b390
Evandro Pires Alves <evandro@ipb.pt>
2019-12-10 17:36:29 +0000

    Fix regular expression: escape '-' using '\-'

--- SimpleHTMLDom.php
+++ SimpleHTMLDom.php
@@ -639,7 +639,7 @@ class simple_html_dom_node
         // This implies that an html attribute specifier may start with an @ sign that is NOT captured by the expression.
         // farther study is required to determine of this should be documented or removed.
         //		$pattern = "/([\w-:\*]*)(?:\#([\w-]+)|\.([\w-]+))?(?:\[@?(!?[\w-]+)(?:([!*^$]?=)[\"']?(.*?)[\"']?)?\])?([\/, ]+)/is";
-        $pattern = "/([\w-:\*]*)(?:\#([\w-]+)|\.([\w-]+))?(?:\[@?(!?[\w-:]+)(?:([!*^$]?=)[\"']?(.*?)[\"']?)?\])?([\/, ]+)/is";
+        $pattern = "/([\w\-:\*]*)(?:\#([\w\-]+)|\.([\w\-]+))?(?:\[@?(!?[\w\-:]+)(?:([!*^$]?=)[\"']?(.*?)[\"']?)?\])?([\/, ]+)/is";
         preg_match_all( $pattern, trim( $selector_string ) . ' ', $matches, PREG_SET_ORDER );
         if ( is_object( $debug_object ) ) {
             $debug_object->debug_log( 2, "Matches Array: ", $matches );
@@ -877,7 +877,7 @@ class simple_html_dom_node
         if ( isset( $this->attr[ 'style' ] ) ) {
             // Thanks to user gnarf from stackoverflow for this regular expression.
             $attributes = array( );
-            preg_match_all( "/([\w-]+)\s*:\s*([^;]+)\s*;?/", $this->attr[ 'style' ], $matches, PREG_SET_ORDER );
+            preg_match_all( "/([\w\-]+)\s*:\s*([^;]+)\s*;?/", $this->attr[ 'style' ], $matches, PREG_SET_ORDER );
             foreach ( $matches as $match ) {
                 $attributes[ $match[ 1 ] ] = $match[ 2 ];
             } //$matches as $match
@@ -1351,7 +1351,7 @@ class simple_html_dom
             $this->char = $this->doc[ --$this->pos ]; // prev
             return true;
         } //$pos = strpos( $tag, '<' ) !== false
-        if ( !preg_match( "/^[\w-:]+$/", $tag ) ) {
+        if ( !preg_match( "/^[\w\-:]+$/", $tag ) ) {
             $node->_[ HDOM_INFO_TEXT ] = '<' . $tag . $this->copy_until( '<>' );
             if ( $this->char === '<' ) {
                 $this->link_nodes( $node, false );
